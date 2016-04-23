57abd2d9027b9f33ff2d30c64aefcb29fb02eb59
Esteban_Moreno <estebanmc2912@gmail.com>
2016-04-23 17:33:36 -0500

    /// iconos y botones con direcciones ///

--- app/src/main/java/bogota/spaceapps/lunaticos/MainActivity.java
+++ app/src/main/java/bogota/spaceapps/lunaticos/MainActivity.java
@@ -1,13 +1,45 @@
 package bogota.spaceapps.lunaticos;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+/*import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+*/
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.botonpre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main3Activity.class));
+            }
+        });
+
+        // Button Botonluna = (Button) findViewById(R.id.Botonluna);
+
+        //Botonluna.setOnClickListener(this)//
     }
 }
+//    public void Reaccionboton (View view){}//    }
+
+  /*  @Override
+    public void onClick(View v) {
+
+
+    }
+}
+*/
